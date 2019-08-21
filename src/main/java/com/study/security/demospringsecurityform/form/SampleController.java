f45e2ce1ca55e382b437d648ad9f427605b2d812
nokchax <nokchax@gmail.com>
2019-08-21 20:33:40 +0900

    스프링 웹 프로젝트 만들기 실습

---
+++ src/main/java/com/study/security/demospringsecurityform/form/SampleController.java
@@ -0,0 +1,40 @@
+package com.study.security.demospringsecurityform.form;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
+
+@Controller
+public class SampleController {
+    @GetMapping("/")
+    public String index(Model model, Principal principal) {
+        if(principal == null) {
+            model.addAttribute("message", "Hello Spring Security");
+        } else {
+            model.addAttribute("message", "Hello, " + principal.getName());
+        }
+
+        return "index";
+    }
+    @GetMapping("/info")
+    public String info(Model model) {
+        model.addAttribute("message", "Info");
+
+        return "info";
+    }
+    @GetMapping("/dashboard")
+    public String dashboard(Model model, Principal principal) {
+        //로그인 하지 않았을 때는 principal 객체는 null이 들어간
+        model.addAttribute("message", "Hello " + principal.getName());
+
+        return "index";
+    }
+    @GetMapping("/admin")
+    public String admin(Model model, Principal principal) {
+        model.addAttribute("message", "Hello Admin " + principal.getName());
+
+        return "admin";
+    }
+}
