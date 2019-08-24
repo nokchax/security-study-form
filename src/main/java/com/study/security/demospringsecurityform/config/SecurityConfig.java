52e4cb864278ccfb495072878cab2f5a672a0b24
nokchax <nokchax@gmail.com>
2019-08-24 21:10:39 +0900

    스프링 시큐리티 커스터마이징: 인메모리 유저 추가

--- src/main/java/com/study/security/demospringsecurityform/config/SecurityConfig.java
+++ src/main/java/com/study/security/demospringsecurityform/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.study.security.demospringsecurityform.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -18,5 +19,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .formLogin()
                 .and()
             .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("nokcha").password("{noop}123").roles("USER")
+                .and()
+                .withUser("admin").password("{noop}!@#").roles("ADMIN");
+        //{noop} 암호화를 하지 않음 no op
+        //{} 내부에는 인코더를 선언하는 것
     }
 }
