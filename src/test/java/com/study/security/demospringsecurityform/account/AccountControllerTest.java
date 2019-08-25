0e03814eb39366d863c327bc3d0674392d465d47
nokchax <nokchax@gmail.com>
2019-08-25 15:15:20 +0900

    스프링 시큐리티 테스트 2장

--- src/test/java/com/study/security/demospringsecurityform/account/AccountControllerTest.java
+++ src/test/java/com/study/security/demospringsecurityform/account/AccountControllerTest.java
@@ -9,8 +9,13 @@ import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -21,6 +26,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class AccountControllerTest {
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private AccountService accountService;
 
     @Test
     public void indexAnonymous() throws Exception {
@@ -65,5 +72,39 @@ public class AccountControllerTest {
         mockMvc.perform(get("/admin").with(user("nokcha").roles("USER")))
                 .andDo(print())
                 .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @Transactional//db rollback을 위해
+    public void login() throws Exception {
+        Account user = createUser("nokchax", "123");
+
+        mockMvc.perform(formLogin().user(user.getUsername()).password(user.getPassword()))
+                .andExpect(authenticated());
+    }
+
+    @Test
+    @Transactional//db rollback을 위해
+    public void loginFail_WrongPw() throws Exception {
+        Account user = createUser("nokchax", "123");
+
+        mockMvc.perform(formLogin().user(user.getUsername()).password("1234"))
+                .andExpect(unauthenticated());
+    }
+
+    private Account createUser(String username, String password) {
+        //save new mock user
+        Account account = new Account();
+        account.setUsername(username);
+        account.setPassword(password);
+        account.setRole("USER");
+
+        accountService.save(account);
+
+        Account dummyAccount = new Account();
+        dummyAccount.setUsername(username);
+        dummyAccount.setPassword(password);
+
+        return dummyAccount;
     }
 }
